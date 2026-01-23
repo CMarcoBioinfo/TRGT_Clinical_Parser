92feffb22b6e71fac112dec2a4c0098214f7a69a
CMarcoBioinfo <corentin.marco.bioinfo@gmail.com>
2026-01-23 11:23:00 +0100

    add remove_zero_repeats, sort_repeats and total_reps to segmentation_utils.py

--- scripts/core/segmentation_utils.py
+++ scripts/core/segmentation_utils.py
@@ -14,3 +14,43 @@ def number_interruptions(interruption_seqs):
 
     # Construction du résultat non trié (le tri sera fait par sort_repeats)
     return "_".join(f"{seq}({count})" for seq, count in nb_inter.items())
+
+
+def remove_zero_repeats(value):
+    if not value:
+        return value
+
+    parts = value.split("_")
+    cleaned = []
+
+    for p in parts:
+        if "(" in p and ")" in p:
+            rep = int(p.split("(")[1].split(")")[0])
+            if rep == 0:
+                continue
+        cleaned.append(p)
+
+    return "_".join(cleaned)
+
+
+def total_reps(p):
+    inside = p.split("(")[1].split(")")[0]
+
+    if "+" in inside:
+        pure, inter = inside.split("+")
+        inter = inter.replace("i", "")
+        return int(pure) + int(inter)
+    else:
+        return int(inside)
+
+
+def sort_repeats(value):
+    if not value or "_" not in value:
+        return value
+
+    parts = value.split("_")
+
+    # tri décroissant sur le nombre total de répétitions
+    parts_sorted = sorted(parts, key=total_reps, reverse=True)
+
+    return "_".join(parts_sorted)
