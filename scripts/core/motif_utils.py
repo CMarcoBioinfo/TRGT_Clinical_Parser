d7021e8df24ee8317ddd372d6a6a5fa806c62082
CMarcoBioinfo <corentin.marco.bioinfo@gmail.com>
2026-01-29 11:55:23 +0100

    update compute_interruption_bp

--- scripts/core/motif_utils.py
+++ scripts/core/motif_utils.py
@@ -21,19 +21,33 @@ def extract_group_motifs(repetitions, interruptions, motif_group):
 def compute_interruption_bp(segmentation, motif_group):
     """
     Calcule :
-      - la taille totale des interruptions internes (i_bp) en bp
-      - le nombre d'interruptions (i_count)
+      - i_bp : taille totale des interruptions internes (en bp)
+      - i_count : nombre d'interruptions internes
+    Une interruption interne est un segment non-groupe situé
+    entre le premier et le dernier segment du motif_group.
     """
+
+    # Indices des segments appartenant au motif_group
+    group_indices = [
+        i for i, (motif, start, end) in enumerate(segmentation)
+        if motif in motif_group
+    ]
+    if not group_indices:
+        return 0, 0
+
+    first_idx = group_indices[0]
+    last_idx = group_indices[-1]
+
     interruption_bp = 0
     interruption_count = 0
 
-    for motif, start, end in segmentation:
+    # Ne parcourir que la fenêtre interne
+    for motif, start, end in segmentation[first_idx:last_idx+1]:
         if motif not in motif_group:
             interruption_bp += (end - start)
             interruption_count += 1
 
     return interruption_bp, interruption_count
-
 
 
 def compute_m(interruption_bp, motif_len):
